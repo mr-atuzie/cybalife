e39cb87319d285518546cec45f5c2e7610994661
overlord <rexatuzie@gmail.com>
2023-02-27 12:33:24 +0100

    mon commit 1

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user");
+const protect = require("../middlewares/authMiddleware");
 
-router.route("/register").post(userController.createUser);
+router.route("/register").post(userController.registerUser);
 
 router.route("/login").post(userController.loginUser);
 
 router.route("/logout").get(userController.logout);
 
-router
-  .route("/:id")
-  .get(userController.getUser)
-  .patch(userController.updateUser)
-  .delete(userController.deleteUser);
+router.route("/login-stat").get(userController.loginStatus);
+
+router.route("/get-user").get(protect, userController.getUser);
+
+router.route("/update-user").patch(protect, userController.updateUser);
+
+router.route("/forgot-password").post(userController.forgotPassword);
+
+router.route("/delete-user").delete(userController.deleteUser);
+
+// router
+//   .route("/:id")
+//   .get(userController.getUser)
+//   .patch(userController.updateUser)
+//   .delete(userController.deleteUser);
 
 module.exports = router;
