149d066df223ce652bd6fd1a84e4569fa882d052
overlord <rexatuzie@gmail.com>
2023-03-01 03:56:13 +0100

    wed commit 1

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,11 @@
 const User = require("../models/User");
+const Token = require("../models/Token");
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+
+const sendEmail = require("../utils/sendEmail");
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
@@ -196,7 +200,53 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 });
 
-const forgotPassword = asyncHandler(async (req, res) => {});
+const forgotPassword = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    res.status(404);
+    throw new Error("User doesn't exist");
+  }
+
+  let resetToken = crypto.randomBytes(32).toString("hex") + user._id;
+
+  const hashToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  await new Token({
+    userId: user._id,
+    token: hashToken,
+    createdAt: Date.now(),
+    expiresAt: Date.now() + 30 * (60 * 1000),
+  }).save();
+
+  const resetLink = `${process.env.FRONTEND_URL}/reset-password/${resetToken}`;
+
+  const message = `
+  <h2>Hi ${user.name}</h2>
+  <p>Please use the url below to reset your password</p>
+  <p>This link expires in 30 minutes</p>
+  <a href=${resetLink} clicktracking=off>${resetLink}</a> 
+  
+  <h6>cybalife</h6>
+  `;
+  const subject = "Reset Email";
+  const send_to = user.email;
+  const send_from = process.env.EMAIL_USER;
+
+  try {
+    await sendEmail(subject, message, send_to, send_from);
+
+    res.status(200).json({ success: true, message: "Reset email sent." });
+  } catch (error) {
+    res.status(500);
+    throw new Error("Email not sent , Please try Again.");
+  }
+});
 
 module.exports = {
   registerUser,
