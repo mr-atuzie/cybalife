0a53489da70c00ce263cdbbfc643d6f177f239d7
overlord <rexatuzie@gmail.com>
2023-06-24 01:03:46 +0100

    grhmj

--- models/User.js
+++ models/User.js
@@ -2,21 +2,12 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    firstname: {
+    name: {
       type: String,
       trim: true,
       required: [true, "Please enter your name"],
     },
-    lastname: {
-      type: String,
-      trim: true,
-      required: [true, "Please enter your name"],
-    },
-    phone: {
-      type: String,
-      trim: true,
-      required: [true, "Please enter your  phone Number"],
-    },
+
     email: {
       type: String,
       trim: true,
@@ -33,55 +24,7 @@ const userSchema = new mongoose.Schema(
       trim: true,
       minLength: [6, "Password must be up to 6 characters"],
     },
-    shippingname: {
-      type: String,
-      default: "",
-    },
-    shippingemail: {
-      type: String,
-      trim: true,
-      match: [
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        "Please enter a valid email",
-      ],
-    },
-    shippingphone: {
-      type: String,
-      trim: true,
-    },
-    country: {
-      type: String,
-      default: "",
-    },
-    state: {
-      type: String,
-      default: "",
-    },
-    city: {
-      type: String,
-      default: "",
-    },
-    address: {
-      type: String,
-      default: "",
-    },
-    desc: {
-      type: String,
-      default: "",
-    },
-    cn: {
-      type: String,
-      default: "",
-    },
-    exp: {
-      type: String,
-      default: "",
-    },
-    cvv: {
-      type: String,
-      default: "",
-    },
-    pin: {
+    photo: {
       type: String,
       default: "",
     },
