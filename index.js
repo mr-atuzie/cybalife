44837a94179e7e451ed02542915a04737f5fde17
overlord <rexatuzie@gmail.com>
2023-07-08 10:53:17 +0100

    hmj,

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.use("/api/v1/house", houseRoutes);
 //Error Middleware
 app.use(errorHandler);
 
-const PORT = 7000;
+const PORT = process.env.PORT || 7000;
 
 const server = app.listen(PORT, () => {
   connectDB();
