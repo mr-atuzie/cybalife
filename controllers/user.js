1cb19bc0437ee4fbdbd7bfd97fa864cf0aed4980
overlord <rexatuzie@gmail.com>
2023-07-08 11:08:45 +0100

    jklj./

--- controllers/user.js
+++ controllers/user.js
@@ -80,7 +80,7 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 
   //Check if user exist
-  const user = await User.findOne({ email: ema });
+  const user = await User.findOne({ email });
 
   if (!user) {
     res.status(400);
